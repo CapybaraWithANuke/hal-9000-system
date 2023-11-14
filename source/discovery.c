40e980c3cb13719dd0eea6d7600de963a3b22fa6
CapybaraWithANuke <dtplusplus@gmail.com>
2023-11-14 12:23:35 +0100

    sockets configuration coded, however binding always gives an error

--- source/discovery.c
+++ source/discovery.c
@@ -9,11 +9,17 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <time.h>
+#include <arpa/inet.h>
 
 #include "../header/commonfuncs.h"
 
 #define ERR_ARGC "There should be exactly 1 argument."
 #define ERR_FD "Configuration file not found"
+
+#define ERR_SOCKET "Socket not created"
+#define ERR_BIND "Binding failed"
+#define ERR_LISTEN "Listening failed"
+#define ERR_ACCEPT "Accepting failed"
 
 #define ERR -1
 
@@ -24,14 +30,56 @@ typedef struct {
     int port_bowman;
 } Config;
 
-Config config;
+int server_poole_fd;
+int server_bowman_fd;
+
+int setupSocket(char* ip, int port, int server_fd) {
+
+    struct sockaddr_in address;
+    socklen_t addrlen;
+    int socket_fd = -1; 
+
+    logn("aboba");
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    logn("bebra");
+    if (server_fd < 0) {
+        logn(ERR_SOCKET);
+        return ERR;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = inet_addr(ip);
+    addrlen = sizeof(address);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        logn(ERR_BIND);
+        return ERR;
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        logn(ERR_LISTEN);
+        return ERR;
+    }
+
+    if ((socket_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+        logn(ERR_ACCEPT);
+        return ERR;
+    }
+
+    return socket_fd;
+
+}
 
 int main(int argc, char** argv) {
 
+    Config config;
     int fd;
 	const char* file_path_start = "config/";
     char* file_path = NULL;
     char* buffer = NULL;
+    int socket_poole_fd = -1;
+    int socket_bowman_fd = -1;
 
     if (argc != 2) {
         logn(ERR_ARGC);
@@ -57,10 +105,21 @@ int main(int argc, char** argv) {
     buffer = read_until(fd, '\n');
     config.port_bowman = atoi(buffer);
 
+    socket_poole_fd = setupSocket(config.ip_poole, config.port_poole, server_poole_fd);
+    socket_bowman_fd = setupSocket(config.ip_bowman, config.port_bowman, server_bowman_fd);
+
+    if (socket_poole_fd == -1 || socket_bowman_fd == -1) {
+        return ERR;
+    }
+
+
     free(config.ip_poole);
     free(config.ip_bowman);
     free(file_path);
     free(buffer);
     close(fd);
+    close(socket_bowman_fd); close(socket_poole_fd);
+    close(server_poole_fd); close(server_bowman_fd);
+    return 0;
 
 }
